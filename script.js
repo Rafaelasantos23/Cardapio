8ceccf9411d43cb285d6a939c08e14dd008bfdae
Rafaela <143849661+Rafaelasantos23@users.noreply.github.com>
2024-05-20 22:19:22 -0300

    resp-395

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("olaaaa mundo")
+let show = true;
+const menuContent = document.querySelector('.content');
+const menuToggle = document.querySelector('.menu-toggle');
+
+
+menuToggle.addEventListener('click', () => {
+
+    menuContent.classList.toggle('on', show);
+    show = !show;
+
+});
